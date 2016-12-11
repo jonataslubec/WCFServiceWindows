e4d2b2e193706aa72854edf1a242640c7a6234ec
Jonatas <Jonatas@JONATAS-PC>
2016-12-11 10:39:49 -0200

    Add project files.

---
+++ IProducts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+
+
+namespace NorthwindServices
+{
+    [ServiceContract]
+    public interface IProducts
+    {
+        [OperationContract] 
+        //[WebInvoke(Method = "GET", 
+        //    ResponseFormat = WebMessageFormat.Json, 
+        //    BodyStyle = WebMessageBodyStyle.Wrapped,
+        //    UriTemplate = "GetProductName/{productID}")]
+        string GetProductName(int productID);
+
+        [OperationContract]
+        int GetProductQty(int productID);
+
+        [OperationContract]
+        string GetCategoryName(int productID);
+
+        [OperationContract]
+        string GetName();
+
+        [OperationContract]
+        string GetDateTime();
+    }
+}
